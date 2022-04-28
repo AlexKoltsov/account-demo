e5cbbba9348595dc88b10316ea7be0d99eb14d18
Alexander Koltsov <alexander.koltsov.work@gmail.com>
2022-04-28 19:42:58 +0300

    More useful logging

--- src/main/java/ru/seven/tech/account/utils/AccountUtils.java
+++ src/main/java/ru/seven/tech/account/utils/AccountUtils.java
@@ -17,7 +17,7 @@ public class AccountUtils {
     public static void decreaseAccountsBalance(Account account, BigDecimal amount) {
         BigDecimal newBalance = account.getBalance().subtract(amount);
         if (newBalance.compareTo(BigDecimal.ZERO) < 0) {
-            throw new NegativeBalanceException("Balance could not be negative");
+            throw new NegativeBalanceException(String.format("Balance of [%s] account could not be negative", account.getId()));
         }
         account.setBalance(newBalance);
     }
